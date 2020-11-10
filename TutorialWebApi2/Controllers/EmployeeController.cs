9062e3dc50e32720b8b92e59a442ff0c04624020
mariannarezk <marianarizek99@gmail.com>
2020-11-10 14:30:10 +0200

    Add project files.

---
+++ TutorialWebApi2/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TutorialWebApi2.Models;
+
+namespace TutorialWebApi2.Controllers
+{
+   
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+        public EmployeeController(DatabaseContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public ActionResult<List<Employee>> Get()
+        {
+            return Ok(_context.Employees.ToList());
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult Get(int id)
+        {
+            var emp = this._context.Employees.Where(m => m.Id == id).FirstOrDefault();
+            return Ok(emp);
+        }
+        [HttpPost]
+        public ActionResult<Employee> post(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+        [HttpPut]
+        public ActionResult<Employee> put(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+
+        public ActionResult<Employee> delete(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return Ok(employee);
+        }
+    }
+}
---
+++ TutorialWebApi2/Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorialWebApi2.Models
+{
+    public class DatabaseContext :DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
